2fd18c963e65a807a8c0fa743c6285dda9d70bbd
Ms. Boba <essentialrandomn3ss@gmail.com>
2022-10-14 22:43:51 -0700

    Update our current roadmap and make it sparkle 🌟 (#36)
    * allow github downloader to also fetch project data
    
    * first draft of getting projects into the roadmap
    
    * Stop github downloader from throwing a fit if it updates too often
    
    * Allow filtering projects by tag
    
    * Add the details of our current projects
    
    * Update our roadmap
    
    * fix broken links
    
    * draft of projects page without updated data
    
    * Update projects page/display
    
    * update projects
    
    * styling
    
    * a bit more css update
    
    Co-authored-by: Essential Randomness <essential.randomn3ss@gmail.com>
    Co-authored-by: catbathingsun <w@we.com>

--- plugins/github-downloader/index.ts
+++ plugins/github-downloader/index.ts
@@ -1,34 +1,131 @@
+import * as dotenv from "dotenv";
+
 import { mkdir, writeFile } from "fs/promises";
 
 import { Octokit } from "@octokit/core";
+import { graphql } from "@octokit/graphql";
 import { paginateRest } from "@octokit/plugin-paginate-rest";
 import path from "path";
+
+// NOTE: .env file will be read from under the docusaurus main folder (since that's
+// where the script is run from)
+dotenv.config();
 
 const DATA_PATH = path.resolve(process.cwd(), "src/_generated_data");
 const octokit = new (Octokit.plugin(paginateRest))();
 
 const destination = "../../";
+const authenticatedGraphqlClient = process.env.GITHUB_TOKEN
+  ? graphql.defaults({
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+    })
+  : null;
 
+// TODO: allow the repo to be passed via configuration
 export default function githubDownloader(context, options) {
   return {
     name: "github-downloader",
     async loadContent() {
-      const labels = await octokit.paginate(
-        "GET /repos/{owner}/{repo}/labels",
+      let labels = null;
+      try {
+        labels = await octokit.paginate(
+          "GET /repos/{owner}/{repo}/labels",
+          {
+            owner: "BobaBoard",
+            repo: "issues",
+          },
+          (response) => response.data
+        );
+      } catch (e) {
+        console.log("Couldn't update labels data.");
+      }
+
+      let projects = null;
+      if (authenticatedGraphqlClient) {
+        const projectsResponse = await authenticatedGraphqlClient(`
         {
-          owner: "BobaBoard",
-          repo: "issues",
-        },
-        (response) => response.data
-      );
-      return { labels };
+          organization(login: "bobaboard") {
+            projectsV2(first: 20) {
+              nodes {
+                id
+                title
+                shortDescription
+                readme
+                url
+                items(first: 20) {
+                  nodes {
+                    creator {
+                      url
+                    }
+                    content {
+                      ... on DraftIssue {
+                        title
+                        body
+                      }
+                      ... on Issue {
+                        id
+                        title
+                        body
+                        url
+                        createdAt
+                        bodyUrl
+                      }
+                      ... on PullRequest {
+                        id
+                        title
+                        body
+                        url
+                        updatedAt
+                      }
+                    }
+                  }
+                }
+                repositories(first: 20) {
+                  nodes {
+                    name
+                    url
+                  }
+                }
+                views(first: 20) {
+                  nodes {
+                    id
+                    name
+                    layout
+                  }
+                }
+              }
+            }
+          }
+        }`);
+        // @ts-expect-error
+        projects = projectsResponse.organization.projectsV2.nodes;
+      } else {
+        console.log(
+          "No authenticated GraphQL client found. Skipping projects update."
+        );
+      }
+      return { labels, projects };
     },
-    async contentLoaded({ content, actions }) {
+    async contentLoaded({ content }) {
+      if (content == null) {
+        console.log("No content found. Skipping update.");
+        return;
+      }
       await mkdir(DATA_PATH, { recursive: true });
-      writeFile(
-        path.resolve(DATA_PATH, "labels.json"),
-        JSON.stringify(content.labels, null, 2)
-      );
+      if (content.labels) {
+        writeFile(
+          path.resolve(DATA_PATH, "labels.json"),
+          JSON.stringify(content.labels, null, 2)
+        );
+      }
+      if (content.projects) {
+        writeFile(
+          path.resolve(DATA_PATH, "projects.json"),
+          JSON.stringify(content.projects, null, 2)
+        );
+      }
     },
   };
 }
