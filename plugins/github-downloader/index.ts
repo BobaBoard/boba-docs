8dce8f3935dc602de5089b09f01251300ab56ba6
Essential Randomness <essential.randomn3ss@gmail.com>
2023-01-20 16:16:57 -0800

    polish up github plugin and don't throw on invalid token

--- plugins/github-downloader/index.ts
+++ plugins/github-downloader/index.ts
@@ -1,118 +1,52 @@
+/* eslint-disable */
 import * as dotenv from "dotenv";
 
 import { mkdir, writeFile } from "fs/promises";
 
-import { Octokit } from "@octokit/core";
-import { graphql } from "@octokit/graphql";
-import { paginateRest } from "@octokit/plugin-paginate-rest";
 import path from "path";
+import { z } from "zod";
+
+const Environment = z.object({
+  GITHUB_TOKEN: z.string().optional(),
+});
 
 // NOTE: .env file will be read from under the docusaurus main folder (since that's
 // where the script is run from)
-dotenv.config();
+Environment.parse(
+  dotenv.config({
+    path: path.resolve(process.cwd(), "./../.env"),
+  })
+);
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof Environment> {}
+  }
+}
+
+import { maybeLoadLabelsFromRepo } from "./labels";
+import { maybeLoadProjectsFromOrg } from "./projects";
 
 const DATA_PATH = path.resolve(process.cwd(), "src/_generated_data");
-const octokit = new (Octokit.plugin(paginateRest))();
-
-const destination = "../../";
-const authenticatedGraphqlClient = process.env.GITHUB_TOKEN
-  ? graphql.defaults({
-      headers: {
-        authorization: `token ${process.env.GITHUB_TOKEN}`,
-      },
-    })
-  : null;
 
 // TODO: allow the repo to be passed via configuration
-export default function githubDownloader(context, options) {
+export default function githubDownloader() {
   return {
     name: "github-downloader",
     async loadContent() {
-      let labels = null;
-      try {
-        labels = await octokit.paginate(
-          "GET /repos/{owner}/{repo}/labels",
-          {
-            owner: "BobaBoard",
-            repo: "issues",
-          },
-          (response) => response.data
-        );
-      } catch (e) {
-        console.log("Couldn't update labels data.");
-      }
+      const labels = await maybeLoadLabelsFromRepo();
+      const projects = await maybeLoadProjectsFromOrg();
 
-      let projects = null;
-      if (authenticatedGraphqlClient) {
-        const projectsResponse = await authenticatedGraphqlClient(`
-        {
-          organization(login: "bobaboard") {
-            projectsV2(first: 20) {
-              nodes {
-                id
-                title
-                shortDescription
-                readme
-                url
-                items(first: 20) {
-                  nodes {
-                    creator {
-                      url
-                    }
-                    content {
-                      ... on DraftIssue {
-                        title
-                        body
-                      }
-                      ... on Issue {
-                        id
-                        title
-                        body
-                        url
-                        createdAt
-                        bodyUrl
-                      }
-                      ... on PullRequest {
-                        id
-                        title
-                        body
-                        url
-                        updatedAt
-                      }
-                    }
-                  }
-                }
-                repositories(first: 20) {
-                  nodes {
-                    name
-                    url
-                  }
-                }
-                views(first: 20) {
-                  nodes {
-                    id
-                    name
-                    layout
-                  }
-                }
-              }
-            }
-          }
-        }`);
-        // @ts-expect-error
-        projects = projectsResponse.organization.projectsV2.nodes;
-      } else {
-        console.log(
-          "No authenticated GraphQL client found. Skipping projects update."
-        );
-      }
       return { labels, projects };
     },
-    async contentLoaded({ content }) {
-      if (content == null) {
-        console.log("No content found. Skipping update.");
-        return;
-      }
+    async contentLoaded({
+      content,
+    }: {
+      content: {
+        labels: Awaited<ReturnType<typeof maybeLoadLabelsFromRepo>>;
+        projects: Awaited<ReturnType<typeof maybeLoadProjectsFromOrg>>;
+      };
+    }) {
       await mkdir(DATA_PATH, { recursive: true });
       if (content.labels) {
         writeFile(
